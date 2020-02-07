c73866748c2dcfe40e3c6aced19708dd398fc33d
VRJunkie1 <mddwebboy@msn.com>
2020-02-06 20:55:18 -0700

    Movement system testing and tweaks
    made a little obstacle course and changed a lot of move stuff

--- Assets/scripts/playerController.cs
+++ Assets/scripts/playerController.cs
@@ -4,14 +4,12 @@ using UnityEngine;
 
 
 [RequireComponent(typeof(Camera))]
-//[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(Animator))]
 [RequireComponent(typeof(Rigidbody))]
 [RequireComponent(typeof(CapsuleCollider))]
 public class playerController : MonoBehaviour
 {
     Rigidbody rigidbody;
-    CapsuleCollider capsulecCollider;
-
 
     Animator animator;
     /// <summary>
@@ -33,25 +31,43 @@ public class playerController : MonoBehaviour
     /// </summary>
     public Transform neckbone;
 
-    public float groundRayRadius = .5f;
-    public float groundRayLength = .1f;
+    public float groundRayRadius = .1f;
+    public float groundRayLength = 1f;
+
+    /// <summary>
+    /// How much influence the rigidbody rotation has on the camera; ie how much it leans when it runs or bounces up hills
+    /// </summary>
+    public float headjostle = .5f;
+
+    /// <summary>
+    /// Character movement will start tweening between grounded and air move past this ground slope. fallSlope is max, and sets isGrounded to false
+    /// </summary>
+    public float struggleSlope = 30;
+    public float fallSlope = 55;
 
     public float walkSpeed = 6.0f;
     public float jumpSpeed = 8.0f;
-    public float gravity = 9.81f;
+    //public float gravity = 9.81f;
+    /// <summary>
+    /// Makes the rigidbody drag on the floor and tilt the player (only on when touching ground)
+    /// </summary>
+    public float walkDownForce = 2;
 
-    public float walkDrag = .8f;    // higher number means less drag; 1 is no drag
+    //public float walkDrag = .8f;    // higher number means less drag; 1 is no drag
     public float walkAccel = .8f;
-    public float airDrag = .95f;
+    //public float airDrag = .95f;
     public float airAccel = .3f;
 
     public float swimSpeed = 10f;
 
-    public float swimDrag = .9f;
+    //public float swimDrag = .9f;
     public float swimAccel = .5f;
 
+    // These should be un-modifyable, but I want to see them in inspector
     public bool isSwimming;
     public bool isGrounded; // feet touching ground
+    public float isGroundedAmount;  // less grounded as slopes get steeper
+    public float groundSlope;
     public bool wasSwimming { get; private set; }
 
     // mouse
@@ -70,7 +86,6 @@ public class playerController : MonoBehaviour
     {
         rigidbody = GetComponent<Rigidbody>();
         animator = GetComponent<Animator>();
-        capsulecCollider = GetComponent<CapsuleCollider>();
         foreach (Transform tf in head.transform)
         {
             if (tf.GetComponent<Camera>() != null) { headCamera = tf.GetComponent<Camera>(); }
@@ -98,21 +113,35 @@ public class playerController : MonoBehaviour
         //print(collision.gameObject.name);
     }
 
+    void applyHeadRotation()
+    {
+        if (isSwimming) head.transform.localRotation = Quaternion.Euler(-swimmingAngle, 0, 0);
+        else head.transform.localRotation = Quaternion.Euler(Y, 0, 0);
+    }
+
     private void FixedUpdate()
     {
         if (inAirZone && !underWaterSurface) isSwimming = false;
         else isSwimming = true;
+        //isSwimming = false;
 
         rigidbody.angularVelocity = Vector3.zero;
+        if (isSwimming) rigidbody.useGravity = false;
+        else rigidbody.useGravity = true;
+
 
         RaycastHit hit;
-        isGrounded = Physics.SphereCast(transform.position, groundRayRadius, -(transform.up), out hit, groundRayLength);
+        isGrounded = Physics.SphereCast(transform.position, groundRayRadius, -(transform.up), out hit, groundRayLength, 1);
         if (isGrounded)
         {
             //transform.rotation = Quaternion.LookRotation(Vector3.Cross(transform.right, hit.normal)   // tilts character along ground
             //print(hit.collider.gameObject.name + Vector3.Angle(hit.normal, new Vector3(0,1,0)));
-            // TODO: Stop character from walking up steep slopes
-        }
+            groundSlope = Vector3.Angle(hit.normal, new Vector3(0, 1, 0));
+            isGroundedAmount = 1 - Mathf.Clamp01((groundSlope - struggleSlope) / (fallSlope - struggleSlope));
+            if (isGroundedAmount == 0) isGrounded = false;
+            //print("isGroundedAmount " + isGroundedAmount + " isGrounded " + isGrounded);
+        } else 
+        { isGroundedAmount = 0; }
 
 
         // Mouse
@@ -129,13 +158,11 @@ public class playerController : MonoBehaviour
         Cursor.lockState = CursorLockMode.Locked;
         //Cursor.visible;
 
-
+        // set rotations
         if (isSwimming)
         {
-            Vector3 oldCamPosition = headCamera.transform.position;
-
             transform.rotation = Quaternion.Euler(Y + swimmingAngle, X, 0.0f);
-            head.transform.localRotation = Quaternion.Euler(-swimmingAngle, 0, 0);
+            //head.transform.localRotation = Quaternion.Euler(-swimmingAngle, 0, 0);
 
             if (!wasSwimming)
             {
@@ -144,26 +171,20 @@ public class playerController : MonoBehaviour
         }
         else
         {
-            Vector3 oldCamPosition = headCamera.transform.position;
-
             transform.rotation = Quaternion.Euler(0, X, 0.0f);
-            head.transform.localRotation = Quaternion.Euler(Y, 0, 0);
+            //head.transform.localRotation = Quaternion.Euler(Y, 0, 0);
 
             if (wasSwimming)
             {
                 headCamera.transform.localPosition = cameraOffsetWalking;
             }
         }
+        applyHeadRotation();
 
-        if (!isSwimming)
-        {
-            if (isGrounded) moveDirection *= walkDrag;
-            else moveDirection *= airDrag;
-        }
-        else moveDirection *= swimDrag;
-
+        // set movement goal
         float accel = walkAccel;
-        if (!isSwimming) { if (!isGrounded) accel = airAccel; }
+        //if (!isSwimming) { if (!isGrounded) accel = airAccel; }
+        if (!isSwimming) { accel = Mathf.Lerp(airAccel, walkAccel, isGroundedAmount); }
         else accel = swimAccel;
 
         Vector3 targetVel;
@@ -180,25 +201,42 @@ public class playerController : MonoBehaviour
         targetVel = Vector3.Normalize(targetVel) * walkSpeed;
         moveDirection = moveDirection * (1- accel) + targetVel * accel;
         
-        if (isGrounded) if (Input.GetButton("Jump")) moveDirection.y = jumpSpeed;
 
-        // Apply gravity. Gravity is multiplied by deltaTime twice (once here, and once below
-        // when the moveDirection is multiplied by deltaTime). This is because gravity should be applied
-        // as an acceleration (ms^-2)
-        if (!isSwimming) moveDirection.y -= gravity * Time.fixedDeltaTime;
 
         // Apply movement
-        rigidbody.velocity = moveDirection;
+        if (isSwimming) rigidbody.velocity = moveDirection;
+        else
+        {
+            rigidbody.velocity = new Vector3(moveDirection.x, rigidbody.velocity.y, moveDirection.z);
+
+            float vertical = Physics.gravity.y * Time.fixedDeltaTime;
+            if(isGrounded) vertical = -walkDownForce;
+            if (Input.GetButton("Jump")) vertical = jumpSpeed * isGroundedAmount;
+            rigidbody.velocity += new Vector3(0, vertical,0);
+        }
+        //rigidbody.AddForce(moveDirection * 5);
         moveOffset = Vector3.zero;
 
         if (isSwimming) animator.SetFloat("speed", moveDirection.magnitude);
         else animator.SetFloat("speed", moveDirection.xz().magnitude);
         animator.SetBool("IsSwimming", isSwimming);
+        animator.SetBool("IsGrounded", isGrounded);
 
         neckbone.localScale = Vector3.zero; // hide the head
 
         inAirZone = false;
         underWaterSurface = false;
         wasSwimming = isSwimming;
+
+
+    }
+
+    private void Update()
+    {
+        // limit head jostle
+        Quaternion headJostled = transform.rotation * Quaternion.Euler(Y, 0, 0);
+        float difference = Quaternion.Angle(headJostled, head.transform.rotation);  // TODO: Use this limit the head jostle harder for more extreme angles
+        //print("diff " + difference);
+        head.transform.rotation = Quaternion.Slerp(Quaternion.Euler(Y, X, 0), headJostled, headjostle);
     }
 }
